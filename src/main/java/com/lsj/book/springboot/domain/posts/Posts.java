7ea2e2c727e68c5b9474d443105c3c5a2f7e522e
lsjboy93 <lsjboy93@gmail.com>
2021-05-22 01:58:39 +0900

    Add register/modify/lookup

--- src/main/java/com/lsj/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/lsj/book/springboot/domain/posts/Posts.java
@@ -19,18 +19,23 @@ public class Posts {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK 생성 규칙. 스프링 부트 2.0버전에선 GenerationType.IDENTITY 옵션을 추가 해야만 auto_increment가 됨
     private Long id;
 
-    @Column(length = 500, nullable = false) //테이블의 칼럼
+    @Column(length = 500, nullable = false) //테이블의 칼럼(안써도 칼럼은 되지만 기본 설정에서 변경을 원할 시 사용한다.
     private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
-    private String author;
+    private String author;  //@Column 어노테이션을 사용하지 않았기 때문에 VARCHAR(255) 기본설정을 사용
 
     @Builder    //해당 클래스의 빌더 패턴 클래스를 생성
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }

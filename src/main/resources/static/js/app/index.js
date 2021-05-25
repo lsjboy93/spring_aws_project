60e7425082e87f633f1b227cd4a58c76c6eb5089
lsjboy93 <lsjboy93@gmail.com>
2021-05-25 10:27:59 +0900

    Update files To delete function

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,12 @@ var main = {
         $('#btn-save').on('click', function() {
             _this.save();
         });
+        $('#btn-update').on('click', function() {   /*btn-update 라는 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 function 실행*/
+            _this.update();
+        });
+        $('#btn-delete').on('click', function() {
+            _this.delete();
+        });
     },
     save : function() {
         var data = {
@@ -24,7 +30,47 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+
+    update : function() {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',    /*HTTP 메소드 중 PUT 메소드 선택. PostsApiController에 있는 API에서 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다.
+                            REST에서 CRUD는 HTTP Method에 매핑된다.(Create - POST, Read - GET, Update - PUT, Delete - DELETE)*/
+            url: '/api/v1/posts/'+id,   /*어느 게시글을 수정할 지 URL Path로 구분하기 위해 Path에 id를 추가*/
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    delete : function() {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function(){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
+
 
 };
 

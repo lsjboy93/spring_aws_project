ac3bf094893d1ec0aff6eb25930c9cc45c66cfe1
lsjboy93 <lsjboy93@gmail.com>
2021-07-22 19:28:29 +0900

    (테스트) 오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 #현재 실행중인 스프링 부트 애플리케이션 종료
 CURRENT_PID=$(pgrep -fl sprint_aws_project | grep jar | awk '{print $1}')
 
-echo "> 현재 구동 중인 애플리케이션pid: $CURRENT_PID"
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
